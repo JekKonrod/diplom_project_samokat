d2c522f6b02c1a81b9d9c62f0a292478f7df4a33
JekKonrod <157355671+JekKonrod@users.noreply.github.com>
2024-02-17 17:18:47 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-# Евгения Асташова, 13-я когорта — Финальный проект. Инженер по тестированию плюс
-
-URL_SERVICE = "https://044d2a34-8019-4c64-8e8e-456b80cadeb3.serverhub.praktikum-services.ru"
-CREATE_ORDER = "/api/v1/orders"
-GET_ORDER_BY_TRACK = "/api/v1/orders/track"
